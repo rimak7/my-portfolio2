fc3b1a4e8f1136ca77dec63a543e3aaa97e1e840
rima <armande.kanm7@gmail.com>
2025-11-29 20:54:22 -0500

    g

---
+++ .github/workflows/test_app.py
@@ -0,0 +1,6 @@
+# content of test_app.py
+
+# A basic test to ensure pytest collects items and runs successfully.
+def test_pytest_runs():
+    """Confirms that the pytest environment is set up correctly."""
+    assert 1 == 1
